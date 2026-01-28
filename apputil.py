cc82cc279f48f066913a2348e47ccb4392df8042
Brody Funk <brodfunk@iu.edu>
2026-01-28 14:21:06 +0000

    commit

--- apputil.py
+++ apputil.py
@@ -1,3 +1,19 @@
-
+def palindrome(word):
+    word = word.lower()
+    word = word.replace(",", "")
+    word = word.replace(".", "")
+    word = word.replace(" ", "")
+    reversed_word = word[::-1]
+    return word == reversed_word 
+    
+result = palindrome("Sit on a potato pan, otis")
+print(result)
 
 # add code below ...
+def parentheses(sequence):
+    if(sequence.startswith("(")) and sequence.endswith(")") and sequence.count('(') == sequence.count(')'):
+        return True
+    else:
+        return False
+
+parentheses("((((((()) ")
